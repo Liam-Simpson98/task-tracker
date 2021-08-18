5634ae5348ab66048ef9b7cf6748f6508cb8f5a7
Liam Simpson <liamrsimpson0319@gmail.com>
2021-08-18 16:07:21 -0600

    Added Json Server for a mock backend

--- src/App.js
+++ src/App.js
@@ -7,26 +7,7 @@ import { useState } from 'react';
 function App() {
   const [showAddTask, setShowAddTask] = useState(false);
 
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'The Zoo',
-      day: 'Feb 5th at 2:30pm',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'The Park',
-      day: 'Feb 7th at 3:30pm',
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: 'The Office',
-      day: 'Feb 9th at 9:00am',
-      reminder: true,
-    },
-  ]);
+  const [tasks, setTasks] = useState([]);
 
   // Add Task
   const addTask = (task) => {
